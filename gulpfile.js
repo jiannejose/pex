d1f24db10aaa3c2f5f254b84fe5d86071aaab1bb
Anne Jizrelle Jose <annejizrelle@gmail.com>
2018-06-20 23:58:36 +0800

    Added Parallax and other js

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,28 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-
-function swallowError (error) {
-
-  console.log(error.toString())
-
-  this.emit('end')
-}
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+const jsLibraries = ['./assets/js/jquery.min.js', './assets/js/parallax.min.js'];
 
 gulp.task('sass', function () {
   return gulp.src('./assets/scss/main.scss')
     .pipe(sass({
       includePaths: require('node-normalize-scss').includePaths
-    }))
+    }).on('error', sass.logError))
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
     .pipe(rename('styles.css'))
     .pipe(sourcemaps.write('./maps'))
-    .on('error', swallowError)
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('watch', function () {
   gulp.watch('./assets/scss/**/*.scss', ['sass']);
+  gulp.watch('./assets/js/*.js', ['copy-js', 'js-compile']);
 });
 
 gulp.task('sass:prod', function () {
@@ -37,4 +33,20 @@ gulp.task('sass:prod', function () {
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename('styles.css'))
     .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('copy-js', function() {
+  return gulp.src(jsLibraries)
+  .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('js-compile', function(cb) {
+  pump([
+    gulp.src('./assets/js/main.js'),
+    babel({
+      presets: ['env']
+    }),
+    uglify(),
+    gulp.dest('./dist/js')
+  ], cb);
 });
